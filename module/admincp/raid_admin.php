9067fb4287e7a73dc76d2f4fb077c773e3a058d7
Alexander Zhovtobryuh <scorpius2005@ukr.net>
2011-12-29 17:10:39 +0200

    добавлено создание гильдий

--- module/admincp/raid_admin.php
+++ module/admincp/raid_admin.php
@@ -11,27 +11,54 @@ require_once(DIR . '/includes/adminfunctions_navpanel.php');
 /**** Default values Section Start  ****/
 error_reporting(E_ALL & ~E_NOTICE);
 if (!can_administer('canadminsettings'))
-	header('Location: /');
+	exit(header('Location: /'));
+
+require_once(DIR.'/raid/class.raid.php');
+define('RAID_PERM', 'admin', true);
+
+$planner = new RaidPlanner($db);
 /**** Default values Section End    ****/
 
 /**** $_GET Section Start           ****/
 /**** $_GET Section End             ****/
 
 /**** $_POST Section Start          ****/
+if (isset($_POST['action']))
+	switch ($_POST['action']) {
+		case 'create_guild':
+			$guild_name = trim(htmlspecialchars($_POST['new_guild'], ENT_QUOTES, 'cp1251'));
+			$planner->createGuild($guild_name);
+			exit(header('Location: raid_admin.php?raid_action=' . $planner->action));
+		case 'delete_guild':
+			$gid = intval($_POST['form_gid']);
+			if ($gid > 0) {
+				$query = sprintf('DELETE FROM %sraid_guilds WHERE id="%d"', TABLE_PREFIX, $gid);
+				$db->query_write($query);
+				$query = sprintf('DELETE FROM %sraid_roster WHERE gid="%d"', TABLE_PREFIX, $gid);
+				$db->query_write($query);
+			}
+			exit(header('Location: raid_admin.php?raid_action=' . $planner->action));
+		case 'save_guild':
+			$gid = intval($_POST['form_gid']);
+			if ($gid > 0) {
+				$gname = trim(htmlspecialchars($_POST['form_name'], ENT_QUOTES, 'cp1251'));
+				$query = sprintf('UPDATE %sraid_guilds SET name="%s" WHERE id="%d"', TABLE_PREFIX, $gname, $gid);
+				$db->query_write($query);
+			}
+			exit(header('Location: raid_admin.php?raid_action=' . $planner->action));
+		default:
+			
+	}
 /**** $_POST Section End            ****/
 
 /**** Default Section Start         ****/
+$planner->createList();
 /**** Default Section End           ****/
 
 /**** Template Section Start        ****/
 print_cp_header();
 can_administer();
 construct_nav_spacer();
-
-require_once(DIR.'/raid/class.raid.php');
-define('RAID_PERM', 'admin', true);
-
-$planner = new RaidPlanner($db);
 
 $planner->getPlannerTemplate('test_admin', DIR . '/raid/templates/admin.html');
 
--- module/raid/class.raid.php
+++ module/raid/class.raid.php
@@ -7,11 +7,14 @@
 class RaidPlanner
 {
 	private $actions = array(
-							 'roster',
-							 'guilds'
-							 );
+	                         'roster',
+	                         'guilds'
+	                         );
 	private $connect;
-	private $action = null;
+	private $guildList = array();
+	private $guild_list = array();
+	
+	public $action = null;
 	
 	/**
 	 * function __construct
@@ -23,6 +26,8 @@ class RaidPlanner
 		$this->connect = $db;
 		if (isset($_GET['raid_action']) && in_array($_GET['raid_action'], $this->actions))
 			$this->action = $_GET['raid_action'];
+		elseif (RAID_PERM == 'admin')
+			exit(header('Location: index.php?do=home'));
 	}
 	
 	/**
@@ -53,7 +58,65 @@ class RaidPlanner
 		
 		$tpl->LoadFile($id, $path);
 		
+		$parse_if = array(
+		                  'is_guild'    => $this->action == 'guilds',
+		                  'is_roster'   => $this->action == 'roster',
+		                  'is_glist'    => count($this->guild_list) > 0,
+		                  );
+		$tpl->parseIf($id, $parse_if);
+		
+		$tpl->parseLoop($id, 'glist', $this->guild_list);
+		
 		$tpl->PrintFile($id);
+	}
+	
+	/**
+	 * function createGuild
+	 * Created 29.12.2011
+	 * @param String $guild_name;
+	 * @result void;
+	 */
+	public function createGuild($guild_name)
+	{
+		$query = sprintf('INSERT INTO %sraid_guilds (name) VALUE ("%s")', TABLE_PREFIX, $guild_name);
+		$this->connect->query_write($query);
+	}
+	
+	/**
+	 * function createList
+	 * Created 29.12.2011
+	 * @result void;
+	 */
+	public function createList()
+	{
+		$function = sprintf('create%sList', ucfirst($this->action));
+		call_user_func(array($this, $function));
+	}
+	
+	/**
+	 * function createGuildsList
+	 * Created 29.12.2011
+	 * @result void;
+	 */
+	private function createGuildsList()
+	{
+		$query = sprintf('SELECT * FROM %sraid_guilds ORDER BY id', TABLE_PREFIX);
+		$result = $this->connect->query_write($query);
+		while (list($gid, $gname) = $this->connect->fetch_row($result))
+			$this->guild_list[] = array(
+			                            'ID'     => $gid,
+			                            'NAME'   => $gname
+			                            );
+	}
+	
+	/**
+	 * function createRosterList
+	 * Created 29.12.2011
+	 * @result void;
+	 */
+	private function createRosterList()
+	{
+		
 	}
 }
 ?>
